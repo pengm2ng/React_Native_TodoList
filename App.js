2ea793c65a978448908da7801a79fd5d99c1d62f
pengm2ng <cjksong@ajou.ac.kr>
2021-07-22 16:32:03 +0900

    end

--- App.js
+++ App.js
@@ -4,6 +4,29 @@ import { StyleSheet, Platform } from 'react-native';
 import styled from 'styled-components/native';
 import Constants from 'expo-constants';
 import _ from 'lodash';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import produce from 'immer';
+
+/*
+//immer 사용법
+const obj =  {a: 1, b: 2 };
+const newObj = produce( obj, draft =>{
+  draft.b = 'b';
+  draft.c = [];
+});
+*/
+/*
+  //---불변성---
+  //원본을 수정하지 않고 사용
+  const array = [];
+  array.map
+  array.filter
+
+  //원본을 수정함.
+  array.push
+  array.pop
+  array.shift, unshift
+*/
 
 const KeyboardAvoidingView = styled.KeyboardAvoidingView`
   flex:1;
@@ -51,58 +74,101 @@ const TempText = styled.Text`
 
 `;
 
+const Check = styled.TouchableOpacity`
+  margin-right: 4px;
+`;
+const CheckIcon = styled.Text`
+  font-size: 20px;
+`;
 
 
 
 
 export default function App() {
   const [list, setList] = React.useState([
-    
+
   ]);
 
-  const [inputTodo, setInputTodo] = React.useState(''); 
-
+  const [inputTodo, setInputTodo] = React.useState('');
   //컴포넌트, 컴포넌트로 이루어진 배열을 리턴할 수 있음.
+
+  //처음 시작할때 저장되있었던 string 목록 가져와서 setList에 넣어주기
+  //ES6 - promise 방식
+  React.useEffect(() => {
+    AsyncStorage.getItem('list')
+      .then(data => {
+        if (data != null) {
+          setList(JSON.parse(data));
+        }
+
+      })
+      .catch(error => {
+        alert(error.message);
+      });
+  }, []);
+
+  const store = (newList) => {
+    setList(newList);
+    AsyncStorage.setItem('list', JSON.stringify(newList));
+  }
+
+
+
+
   return (
-    <Container> 
+    <Container>
       <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
 
         <Contents>
           {list.map(item => {
             return (
               <TodoItem key={item.id}>
+                <Check onPress ={()=>{
+                  store(produce(list, draft=>{
+                    const index = list.indexOf(item);
+                    draft[index].done=!list[index].done;
+                    alert(draft[index].done)
+                  }));
+                }}>
+                  <CheckIcon>
+                    {item.done ? '✅' : '☑️'}
+                    </CheckIcon>
+                </Check>
                 <TodoItemText >
+               
                   {item.todo}
                 </TodoItemText>
-                <TodoItemButton title="삭제" 
-                onPress={() => { 
-                  const rejectedList = _.reject(list, element => element.id === item.id)
-                  setList(rejectedList);
+                <TodoItemButton title="삭제"
+                  onPress={() => {
+                    const rejectedList = new _.reject(list, element => element.id === item.id)
+                    store(rejectedList);
 
-                }} />
+                  }} />
               </TodoItem>
             )
           })}
 
         </Contents>
         <InputContainer>
-          <Input value = { inputTodo } onChangeText ={ value => setInputTodo(value) } />
-          <Btn title="전송" 
-                    onPress={() => { 
-                      if( inputTodo ===''){
-                        return;
-                      }
-                      const newItem = {
-                        id: new Date().getTime().toString(),
-                        todo: inputTodo,
+          <Input value={inputTodo} onChangeText={todo => setInputTodo(todo)} />
 
-                      };
-                      setList([
-                        ...list, //전개 연산자 spread operator
-                        newItem,
-                      ]);
-                      setInputTodo('')
-                    }} />
+          <Btn title="전송"
+            onPress={() => {
+              if (inputTodo === '') {
+                return;
+              }
+              const newItem = {
+                id: new Date().getTime().toString(),
+                todo: inputTodo,
+                done : false,
+
+              };
+              store([
+                ...list, //전개 연산자 spread operator
+                newItem,
+              ]);
+              setInputTodo('');
+            }} />
 
         </InputContainer>
       </KeyboardAvoidingView>
@@ -111,10 +177,5 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
